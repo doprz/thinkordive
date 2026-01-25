e8ff610ec05a3be01636cba1910da6b92f17b52c
doprz <52579214+doprz@users.noreply.github.com>
2026-01-24 22:55:23 -0600

    feat: add Nix flake and direnv

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A full-stack stock trading simulation platform where users can buy and sell stocks in a realistic market environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = inputs.nixpkgs.lib.systems.flakeExposed;
+
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            name = "thinkorswim-dev";
+
+            packages = with pkgs; [
+              bun
+              turbo
+            ];
+          };
+        };
+    };
+}
