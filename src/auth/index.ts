c1951f7b203303f5aad850bae22e20916e1e1487
doprz <52579214+doprz@users.noreply.github.com>
2026-03-20 00:17:48 -0500

    fix: better auth in prod

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -9,9 +9,12 @@ import { db } from "@/server/db/init";
 // Instead, we use a singleton so auth is only created once on the first request rather than re-instantiated on every call.
 function createAuth() {
   return betterAuth({
-    database: drizzleAdapter(db, {
-      provider: "sqlite",
-    }),
+    // NOTE: Better Auth added Cloudflare D1 Support in v1.5
+    // See: https://better-auth.com/blog/1-5#cloudflare-d1-support
+    // database: env.thinkordive_db,
+    // BUG: Using the native d1 support causes:
+    // (log) kysely:warning: outdated driver/plugin detected! `QueryResult.numUpdatedOrDeletedRows` has been replaced with `QueryResult.numAffectedRows`.
+    database: drizzleAdapter(db, { provider: "sqlite" }),
     emailAndPassword: {
       enabled: true,
     },
